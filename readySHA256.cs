a2c600c023db25dfb4101090499dde1cbf0cfc89
Atabey Aydı <64842894+Libldeaera@users.noreply.github.com>
2022-01-15 09:10:24 +0300

    Create readySHA256.cs

---
+++ readySHA256.cs
@@ -0,0 +1,37 @@
+using System;  
+using System.Text;  
+using System.Security.Cryptography;  
+  
+namespace HashConsoleApp  
+{  
+    class Program  
+    {  
+        static void Main(string[] args)  
+        {  
+            string plainData = "1928+Macaristan+02165879800";  
+            Console.WriteLine("Raw data: {0}", plainData);  
+            string hashedData = ComputeSha256Hash(plainData);  
+            Console.WriteLine("Hash\n{0}", hashedData);  
+            Console.WriteLine(ComputeSha256Hash("1928+Macaristan+02165879800"));  
+            Console.ReadLine();  
+        }  
+  
+        static string ComputeSha256Hash(string rawData)  
+        {  
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())  
+            {  
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));  
+  
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();  
+                for (int i = 0; i < bytes.Length; i++)  
+                {  
+                    builder.Append(bytes[i].ToString("x2"));  
+                }  
+                return builder.ToString();  
+            }  
+        }               
+    }  
+}  
